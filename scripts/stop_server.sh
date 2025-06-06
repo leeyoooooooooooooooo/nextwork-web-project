60c83d4ee3010c97e4dcb0a5346b5b78a7095777
EC2 Default User <ec2-user@ip-172-31-26-175.ap-southeast-1.compute.internal>
2025-06-06 12:27:50 +0000

    Adding CodeDeploy files

---
+++ scripts/stop_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+isExistApp="$(pgrep httpd)"
+if [[ -n $isExistApp ]]; then
+sudo systemctl stop httpd.service
+fi
+isExistApp="$(pgrep tomcat)"
+if [[ -n $isExistApp ]]; then
+sudo systemctl stop tomcat.service
+fi
+#!/bin/bash
+isExistApp="$(pgrep httpd)"
+if [[ -n $isExistApp ]]; then
+sudo systemctl stop httpd.service
+fi
+isExistApp="$(pgrep tomcat)"
+if [[ -n $isExistApp ]]; then
+sudo systemctl stop tomcat.service
+fi
